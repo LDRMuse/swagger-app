0b327646ee5065251082e0966a599891c1b7e39e
Melissa <melissakheying@gmail.com>
2020-10-28 12:35:52 -0500

    :building_construction: bring in packages and leverage
    bring in yamljs as YAML and swagger-ui-express as swagger. Use YAML to load the swagger document as swaggerDocument. Then use express to declare the route, and use swaggerUi to call the method serve and call the method setup to setup or use the swaggerDocument.

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+
+const YAML = require('yamljs')
+const swaggerUI = require('swagger-ui-express')
+const swaggerDocument = YAML.load('./api/swagger/swagger.yaml')
+
 module.exports = app; // for testing
 
 var config = {
@@ -10,6 +15,8 @@ var config = {
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
+
+  app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
   // install middleware
   swaggerExpress.register(app);
