9d6742d605fbbaceb5ce8d6183eb9bf8c22f8d67
Melissa <melissakheying@gmail.com>
2020-10-30 12:20:17 -0500

    :recycle: refactor POST request
    place publicRecipeList globally for all functions to use. place publicRecipeList in appropriate places

--- api/controllers/recipes.js
+++ api/controllers/recipes.js
@@ -1,9 +1,10 @@
 // side-note; it's best to describe arrays as lists for a quick reminder that the list is an array
 const Recipe = require('../models/Recipe');
 
+let publicRecipeList = []
+
 module.exports.getRecipe = (_, res) => {
   let recipes = new Recipe.getRecipeList()
-  let publicRecipeList = []
 
   for (let recipe of recipes) {
     if (recipe.isPublic) {
@@ -15,9 +16,9 @@ module.exports.getRecipe = (_, res) => {
 
 module.exports.postRecipe = (req, res) => {
   let newRecipe = req.body
-  newRecipe.id = recipeList.length + 1
+  newRecipe.id = publicRecipeList.length + 1
   newRecipe.isPublic = true
-  recipeList.push(newRecipe)
+  publicRecipeList.push(newRecipe)
 
   return res.status(204).end()
 }
